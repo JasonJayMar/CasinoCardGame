4ba345522835936442f8266a601b4f3dd3278def
Jason Martinez <39502339+JasonJayMar@users.noreply.github.com>
2018-07-15 19:30:18 -0400

    Add files via upload

---
+++ casino-fullstack/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://casino:casino1@ds237641.mlab.com:37641/casino' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
